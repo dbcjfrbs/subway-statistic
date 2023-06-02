e2ec32e2b6718e29febf96565e2a3c0648c95426
kimmanjoong <enne123@sugarbricks.co.kr>
2023-06-02 20:49:25 +0900

    계산 끗

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useEffect, useState} from 'react';
+import { CSSProperties, useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,10 +7,10 @@ import {
   Title,
   Tooltip,
   Legend,
-  ChartData
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import './App.css'
+  ChartData,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import "./App.css";
 
 ChartJS.register(
   CategoryScale,
@@ -25,24 +25,30 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top' as const,
+      position: "top" as const,
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart',
+      text: "Chart.js Bar Chart",
     },
   },
 };
 
-async function getData(dow:string, hh:string): Promise<{label:Array<string>, data:Array<number>}>{
-  const url = `/puzzle/congestion-train/stat/stations/219?dow=${dow}&hh=${hh}`; // 삼성역 
+async function getData(
+  dow: string,
+  hh: string
+): Promise<{ label: Array<string>; data: Array<number> }> {
+  const url = `/puzzle/congestion-train/stat/stations/219?dow=${dow}&hh=${hh}`; // 삼성역
   const options = {
-    method: 'GET',
-    headers: {accept: 'application/json', appkey: 'EiGninSCwg3HK7MB72kfD34BNljEJZ1R7nawXwlV'}
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      appkey: "EiGninSCwg3HK7MB72kfD34BNljEJZ1R7nawXwlV",
+    },
   };
   // let label:Array<string>;
   // let data:Array<string>;
- 
+
   const response = await fetch(url, options);
   // .then(res => res.json())
   // .then(json => {
@@ -50,88 +56,87 @@ async function getData(dow:string, hh:string): Promise<{label:Array<string>, dat
   // if (!response.ok){
   //   return response.status
   // }
-  console.log(response)
+  console.log(response);
   const data = await response.json();
-  console.log(data)
+  console.log(data);
   // const stat: {hh: string, mm: string, congestionTrain: number}[] = data.contents.stat[0].data ;
 
-  let label:any;
-  let chartData:any;
+  let label: any[] = [];
+  let chartData: any = [];
   // const zero_congest=[0, 0, 0, 0, 0, 0];
-  const stat: any = data.contents.stat ;
-  let len=stat.length()
-  for(let i=0; i<len; i++){
-    const tmp1: {hh: string, mm: string, congestionTrain: number}[]=stat[i].data;
-    // label 생성
-    const label_sub= tmp1.map(({hh, mm})=>
-      hh+mm
-    )
-    // data 생성
-    const chartData_sub = tmp1.map(({congestionTrain})=>
-      congestionTrain
-    )
+  const { stat }: { stat: any[] } = data.contents;
+  const datas = stat.reduce((res: Record<string, number[]>, { data }: any) => {
+    const obj = { ...res };
+    const temp = data.reduce(
+      (acc: Record<string, number>, { hh, mm, congestionTrain }: any) => ({
+        ...acc,
+        [`${hh}${mm}`]: congestionTrain,
+      }),
+      {}
+    );
 
-    // 배열 내 원소 모두 0이면 스킵
-    if(chartData_sub.every((item)=>item === 0)) continue;
-    
-    label.push(...label_sub);
-    chartData.push(...chartData_sub);
-    [['0800', 18],['0810', 18],['0820', 18],['0830', 18],['0840', 18],['0850', 18], ...,
-    ['0800', 18],['0810', 18],['0820', 18],['0830', 18],['0840', 18],['0850', 18],]
+    for (const key in temp) {
+      if (temp[key] && temp[key] !== 0) {
+        const [sum, count] = obj[key] ?? [0, 0];
+        obj[key] = [sum + temp[key], count + 1];
+      }
+    }
+
+    return obj;
+  }, {});
+  const result: number[] = [];
+  for (const key in datas) {
+    const [sum, cnt] = datas[key];
+    result.push(Math.round(sum / cnt));
   }
 
-  // 각 시간별로 평균내기
-  len=label.length();
-    
-  for(let i=0; i<len; i++){
-    
-  }
-// })
-  return {label, data: chartData};
+  console.log(result);
+  // })
+  return {
+    label: Object.keys(datas),
+    data: result,
+  };
 }
 
 export default function App() {
-  const [chart, setChart] = useState<ChartData<'bar'> | null>(null)
+  const [chart, setChart] = useState<ChartData<"bar"> | null>(null);
   useEffect(() => {
     // 06:00 - 23:00
-    (async()=>{
+    (async () => {
       const labels: string[] = [];
       const datas: number[] = [];
-      for (let i = 6; i < 24; i++) {
-        // const {l, d} = await Promise.all([getData('MON', '6'), getData('MON', '7'), getData('MON', '8')]).then()
-        // const seriesResult = await Promise.all(Array.from({length: 24 - 6}, (_, idx)=> getData('MON', `${ idx + 6 < 10 ? `0${idx + 6}` : String(idx + 6)}`)))
-        // seriesResult.forEach(({label, data})=>{
-        //   labels.push(...label)
-        //   datas.push(...data)
-        // })
-
-        const { label, data } = await getData('MON', `${ i < 10 ? `0${i}` : String(i)}`)
-        labels.push(...label)
-        datas.push(...data)        
+      for (let i = 6; i < 7; i++) {
+        const { label, data } = await getData(
+          "MON",
+          `${i < 10 ? `0${i}` : String(i)}`
+        );
+        labels.push(...label);
+        datas.push(...data);
       }
-      
-      console.log(labels, datas)
-      const chartData: ChartData<'bar'> = {
+
+      const chartData: ChartData<"bar"> = {
         labels,
         datasets: [
           {
-            label: '지역명',
+            label: "지역명",
             data: datas,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
           },
         ],
       };
       setChart(chartData);
-    })()
+    })();
   }, []);
 
-  return <>{chart ?
-  <Bar className='서울역 지하철혼잡도' options={options} data={chart} />
-  : 
-  null}</>
+  return (
+    <>
+      {chart ? (
+        <Bar className="서울역 지하철혼잡도" options={options} data={chart} />
+      ) : null}
+    </>
+  );
 }
 
 function setStats(json: any) {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
-
